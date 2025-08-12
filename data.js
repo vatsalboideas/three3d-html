65e14178ffa58ee364d713fbee9e43cb291fbfed
vatsalboideas <vatsal.soni@boideas.com>
2025-08-12 20:49:02 +0530

    all views added

--- data.js
+++ data.js
@@ -14,160 +14,490 @@
  * limitations under the License.
  */
 var data = {
-  "scenes": [
+  scenes: [
     {
-      "id": "oriente-station",
-      "name": "Entry Scene",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+      id: "oriente-station",
+      name: "Entry Scene",
+      images: {
+        low: "/images/1.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/1.png",
       },
-      "linkHotspots": [
+      levels: [
         {
-          "yaw": 3.12678386676067,
-          "pitch": -0.0076340532339251865,
-          "rotation": 0,
-          "target": "electricity-museum"
-        }
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
       ],
-      "infoHotspots": [
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
         {
-          "yaw": -0.00038049728702915786,
-          "pitch": 0.014985751462495145,
-          "title": "Oriente Station",
-          "text": "The Oriente Station is one of the most important bus and train stations in the city. Designed by the Spanish architect and engineer Santiago Calatrava, it has an enormous metal skeleton that covers the eight train lines and its platforms. Finished in 1998 to serve the Expo’98 and, later, the Parque das Nações area, in its surroundings are companies, services, hotels, bars, animation, as well as the well known Vasco da Gama shopping centre."
-        }
-      ]
+          yaw: 3.12678386676067,
+          pitch: -0.0076340532339251865,
+          rotation: 0,
+          target: "electricity-museum",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: -0.00038049728702915786,
+          pitch: 0.014985751462495145,
+          title: "Oriente Station",
+          text: "The Oriente Station is one of the most important bus and train stations in the city. Designed by the Spanish architect and engineer Santiago Calatrava, it has an enormous metal skeleton that covers the eight train lines and its platforms. Finished in 1998 to serve the Expo’98 and, later, the Parque das Nações area, in its surroundings are companies, services, hotels, bars, animation, as well as the well known Vasco da Gama shopping centre.",
+        },
+      ],
     },
     {
-      "id": "electricity-museum",
-      "name": "Pre Function Area 1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+      id: "electricity-museum",
+      name: "Pre Function Area 1",
+      images: {
+        low: "/images/2.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/2.png",
       },
-      "linkHotspots": [
+      levels: [
         {
-          "yaw": -2.3152585099587224,
-          "pitch": 0.045251205931975846,
-          "rotation": 5.497787143782138,
-          "target": "jeronimos"
-        }
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
       ],
-      "infoHotspots": [
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
         {
-          "yaw": -0.1606464893205768,
-          "pitch": -0.17433292221669205,
-          "title": "Boilers Room",
-          "text": "In the impressive Boilers Room at the Electricity Museum we find four large boilers of about 100 feet tall, with their respective control panels, air and fuel circuits, ventilators, etc. Boiler number 15 has been musealised and visitors may go in and discover its structure and internal component: conveyor belt, Bailey walls, naphtha burners, water heating tubes, and so on."
-        }
-      ]
+          yaw: -2.3152585099587224,
+          pitch: 0.045251205931975846,
+          rotation: 5.497787143782138,
+          target: "jeronimos",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: -0.1606464893205768,
+          pitch: -0.17433292221669205,
+          title: "Boilers Room",
+          text: "In the impressive Boilers Room at the Electricity Museum we find four large boilers of about 100 feet tall, with their respective control panels, air and fuel circuits, ventilators, etc. Boiler number 15 has been musealised and visitors may go in and discover its structure and internal component: conveyor belt, Bailey walls, naphtha burners, water heating tubes, and so on.",
+        },
+      ],
     },
     {
-      "id": "jeronimos",
-      "name": "Pre Function Area 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+      id: "jeronimos",
+      name: "Pre Function Area 2",
+      images: {
+        low: "/images/3.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/3.png",
       },
-      "linkHotspots": [
+      levels: [
         {
-          "yaw": -0.775981148319735,
-          "pitch": 0.2661802812323746,
-          "rotation": 0,
-          "target": "oriente-station"
-        }
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
       ],
-      "infoHotspots": [
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
         {
-          "yaw": 0.5350080558065997,
-          "pitch": 0.24525106321929435,
-          "title": "Jerónimos Monastery",
-          "text": "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor."
-        }
-      ]
-    }
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
+    {
+      id: "preFunctionArea3",
+      name: "Pre Function Area 3",
+      images: {
+        low: "/images/4.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/4.png",
+      },
+      levels: [
+        {
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
+      ],
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
+        {
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
+    {
+      id: "preFunctionArea4",
+      name: "Pre Function Area 4",
+      images: {
+        low: "/images/5.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/5.png",
+      },
+      levels: [
+        {
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
+      ],
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
+        {
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
+    {
+      id: "centerView",
+      name: "Center View",
+      images: {
+        low: "/images/center.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/center.png",
+      },
+      levels: [
+        {
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
+      ],
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
+        {
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
+    {
+      id: "stageView",
+      name: "Stage View",
+      images: {
+        low: "/images/stage.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/stage.png",
+      },
+      levels: [
+        {
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
+      ],
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
+        {
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
+    {
+      id: "backView",
+      name: "Back View",
+      images: {
+        low: "/images/back.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/back.png",
+      },
+      levels: [
+        {
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
+      ],
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
+        {
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
+    {
+      id: "blankView",
+      name: "Blank View",
+      images: {
+        low: "/images/blank.webp",
+        high: "https://fourseasons-tril.s3.ap-south-1.amazonaws.com/blank.png",
+      },
+      levels: [
+        {
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
+      ],
+      faceSize: 4096,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [
+        {
+          yaw: -0.775981148319735,
+          pitch: 0.2661802812323746,
+          rotation: 0,
+          target: "oriente-station",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 0.5350080558065997,
+          pitch: 0.24525106321929435,
+          title: "Jerónimos Monastery",
+          text: "The Jerónimos Monastery cloister is a pleasant and serene place intended to foster monks’ prayers and meditation. Its manuelin decoration features decorative religious, nautical and royal elements, as well as vegetal motifs. Since 1985, the tomb of the poet Fernando Pessoa rests in the north wing of the cloister’s ground floor.",
+        },
+      ],
+    },
   ],
-  "name": "Sample Tour",
-  "settings": {
-    "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
-  }
+  name: "Sample Tour",
+  settings: {
+    mouseViewMode: "drag",
+    autorotateEnabled: true,
+    fullscreenButton: true,
+    viewControlButtons: true,
+  },
 };
